8004c6b0d34fbc40ec8879353d8cbc83232348f6
Stephen Johnson <stevej@ucla.edu>
2016-08-31 12:20:07 -0700

    Apply my customizations

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -179,3 +179,27 @@ alias gwc='git clean -n'
 alias gwC='git clean -f'
 alias gwx='git rm -r'
 alias gwX='git rm -rf'
+
+
+
+
+# http://www.jperla.com/blog/post/teach-yourself-git-in-2-minutes
+alias ad='git add'
+alias pl='git pull'
+alias ph='git push'
+alias cm='git commit'
+alias sl='git status -uall'
+alias lg='git log'
+alias gp='git grep'
+alias de='git diff --ignore-space-change'
+alias me='git merge'
+alias bh='git branch'
+alias ct='git checkout'
+# alias cm='git commit -m'
+alias glog='git gr'
+
+git config --global color.ui true
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.status auto
+
